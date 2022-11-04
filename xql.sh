bec2b3e813ac16be539b7a1c271d5a83cc48d2b4
281677160 <66651997+281677160@users.noreply.github.com>
2022-11-04 18:36:43 +0800

    Update xql.sh

--- xql.sh
+++ xql.sh
@@ -51,14 +51,11 @@ rm -f /tmp/luci-indexcache
 
 mv /etc/openwrt_banner /etc/banner
 
-sed -i '/DISTRIB_RELEAS/d' /etc/openwrt_release
-echo "DISTRIB_RELEASE='SNAPSHOT'" >> /etc/openwrt_release
-sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
-echo "DISTRIB_REVISION='20.22'" >> /etc/openwrt_release
-sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
+
 echo "DISTRIB_DESCRIPTION='OpenWrt '" >> /etc/openwrt_release
-sed -i '/luciname/d' /usr/lib/lua/luci/version.lua
 sed -i '/luciversion/d' /usr/lib/lua/luci/version.lua
-echo "luciname    = \"Immortalwrt-20.22\"" >> /usr/lib/lua/luci/version.lua
+echo "luciversion    = \"Immortalwrt\"" >> /usr/lib/lua/luci/version.lua
+sed -i '/luciname/d' /usr/lib/lua/luci/version.lua
+echo "luciname    = \"- master\"" >> /usr/lib/lua/luci/version.lua
 
 exit 0
