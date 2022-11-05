66b19b6a993b986aad58c5799e4af7d01fdeb9a4
281677160 <66651997+281677160@users.noreply.github.com>
2022-11-05 10:58:06 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -500,6 +500,7 @@ fi
 
 
 function Diy_Language() {
+sed -i '/^#/d' ${FIN_PATH}
 if [[ "$(. ${BASE_PATH}/etc/openwrt_release && echo "$DISTRIB_RECOGNIZE")" != "18" ]]; then
   echo "正在执行：把插件语言转换成zh_Hans"
   cd ${HOME_PATH}
