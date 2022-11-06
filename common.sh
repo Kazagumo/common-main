f154c360befb169d45fa228bf87f0fa3551f266d
281677160 <66651997+281677160@users.noreply.github.com>
2022-11-06 22:53:21 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -466,13 +466,11 @@ function Diy_chajianyuan() {
 echo "正在执行：给feeds.conf.default增加插件源"
 # 这里增加了源,要对应的删除/etc/opkg/distfeeds.conf插件源
 echo "
+src-git danshui https://github.com/281677160/openwrt-package.git;${PACKAGE_BRANCH}
 src-git helloworld https://github.com/fw876/helloworld
 src-git passwall https://github.com/xiaorouji/openwrt-passwall;packages
 src-git passwall1 https://github.com/xiaorouji/openwrt-passwall;luci
 src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2;main
-src-git danshui https://github.com/281677160/openwrt-package.git;${PACKAGE_BRANCH}
-src-git nas https://github.com/linkease/nas-packages.git;master
-src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main
 " >> ${HOME_PATH}/feeds.conf.default
 sed -i '/^#/d' "${HOME_PATH}/feeds.conf.default"
 sed -i '/^$/d' "${HOME_PATH}/feeds.conf.default"
