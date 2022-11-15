b9422e1ccc1e0526fa2b903f748d6054b3a8b84e
281677160 <66651997+281677160@users.noreply.github.com>
2022-11-15 23:27:13 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -472,8 +472,9 @@ if [[ "${REPO_BRANCH}" = "openwrt-21.02" ]]; then
   bash -c "$(curl -fsSL https://raw.githubusercontent.com/281677160/common-main/main/LIENOL/19.07/package/kernel/linux/modules/netsupport.sh)"
 elif [[ "${REPO_BRANCH}" = "openwrt-19.07" ]]; then
   sed -i "s?+luci-lib-base?+luci-base?g" ${HOME_PATH}/package/default-settings/Makefile
-  rm -rf feeds/packages/devel/packr && svn export https://github.com/281677160/common-main/tree/main/OFFICIAL/1907/packr feeds/packages/devel/packr
-  rm -rf feeds/packages/libs/libcap && svn co https://github.com/coolsnowwolf/lede/trunk/package/libs/libcap feeds/packages/libs/libcap
+  rm -rf ${HOME_PATH}/feeds/packages/lang/golang && svn export https://github.com/coolsnowwolf/packages/trunk/lang/golang ${HOME_PATH}/feeds/packages/lang/golang
+  rm -rf ${HOME_PATH}/feeds/packages/devel/packr && svn export https://github.com/281677160/common-main/trunk/OFFICIAL/1907/packr ${HOME_PATH}/feeds/packages/devel/packr
+  rm -rf ${HOME_PATH}/feeds/packages/libs/libcap && svn co https://github.com/coolsnowwolf/lede/trunk/package/libs/libcap ${HOME_PATH}/feeds/packages/libs/libcap
   rm -rf ${HOME_PATH}/package/libs/libpcap && svn export https://github.com/coolsnowwolf/lede/trunk/package/libs/libpcap ${HOME_PATH}/package/libs/libpcap
 fi
 
