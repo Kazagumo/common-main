b33392779e0dce1902fded86c17719020f0484db
281677160 <66651997+281677160@users.noreply.github.com>
2022-11-16 10:21:19 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -204,17 +204,12 @@ echo "正在执行：判断是否缺少[${CONFIG_FILE}、${DIY_PART_SH}]文件"
 
 
 function Diy_update() {
-if [[ ! ${bendi_script} == "1" ]]; then
-  export INS="sudo -E apt-get -qq"
-else
-  export INS="sudo apt-get"
-fi
-${INS} update -y
-${INS} full-upgrade -y
-${INS} -y install build-essential asciidoc binutils bzip2 curl gawk gettext git libncurses5-dev libz-dev patch python3.5 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf
-${INS} install -y rename
-${INS} autoremove -y --purge
-${INS} clean
+sudo apt-get -y update
+sudo apt-get -y full-upgrade
+sudo apt-get -y install build-essential asciidoc binutils bzip2 curl gawk gettext git libncurses5-dev libz-dev patch python3.5 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf
+sudo apt-get -y install rename
+sudo apt-get -y autoremove --purge
+sudo apt-get -y clean
 }
 
 
