32c707aabd0922054283543b76c35e0e547e8aa6
281677160 <66651997+281677160@users.noreply.github.com>
2022-11-16 16:12:54 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -210,7 +210,10 @@ echo "正在执行：判断是否缺少[${CONFIG_FILE}、${DIY_PART_SH}]文件"
 
 
 function Diy_update() {
-sudo apt-get -y update
+sudo -E apt-get -qq update
+sudo -E apt-get -qq install $(curl -fsSL https://is.gd/depend_ubuntu2204_openwrt)
+sudo -E apt-get -qq autoremove --purge
+sudo -E apt-get -qq clean
 }
 
 
