e47a38a67f577f71634e24d435d5c7139611e354
281677160 <66651997+281677160@users.noreply.github.com>
2022-11-19 09:59:09 +0800

    Update netsupport.sh

--- LIENOL/19.07/package/kernel/linux/modules/netsupport.sh
+++ LIENOL/19.07/package/kernel/linux/modules/netsupport.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 if [[ `grep -c "KernelPackage/inet-diag" package/kernel/linux/modules/netsupport.mk` -eq '0' ]]; then
-echo '
+echo "
 define KernelPackage/netlink-diag
-  SUBMENU:=$(NETWORK_SUPPORT_MENU)
+  SUBMENU:=\$(NETWORK_SUPPORT_MENU)
   TITLE:=Netlink diag support for ss utility
   KCONFIG:=CONFIG_NETLINK_DIAG
-  FILES:=$(LINUX_DIR)/net/netlink/netlink_diag.ko
-  AUTOLOAD:=$(call AutoLoad,31,netlink-diag)
+  FILES:=\$(LINUX_DIR)/net/netlink/netlink_diag.ko
+  AUTOLOAD:=\$(call AutoLoad,31,netlink-diag)
 endef
 
 define KernelPackage/netlink-diag/description
  Netlink diag is a module made for use with iproute2's ss utility
 endef
 
-$(eval $(call KernelPackage,netlink-diag))
+\$(eval \$(call KernelPackage,netlink-diag))
 
 define KernelPackage/inet-diag
-  SUBMENU:=$(NETWORK_SUPPORT_MENU)
+  SUBMENU:=\$(NETWORK_SUPPORT_MENU)
   TITLE:=INET diag support for ss utility
-  KCONFIG:= \
-	CONFIG_INET_DIAG \
-	CONFIG_INET_TCP_DIAG \
-	CONFIG_INET_UDP_DIAG \
-	CONFIG_INET_RAW_DIAG \
+  KCONFIG:= \\
+	CONFIG_INET_DIAG \\
+	CONFIG_INET_TCP_DIAG \\
+	CONFIG_INET_UDP_DIAG \\
+	CONFIG_INET_RAW_DIAG \\
 	CONFIG_INET_DIAG_DESTROY=n
-  FILES:= \
-	$(LINUX_DIR)/net/ipv4/inet_diag.ko \
-	$(LINUX_DIR)/net/ipv4/tcp_diag.ko \
-	$(LINUX_DIR)/net/ipv4/udp_diag.ko \
-	$(LINUX_DIR)/net/ipv4/raw_diag.ko
-  AUTOLOAD:=$(call AutoLoad,31,inet_diag tcp_diag udp_diag raw_diag)
+  FILES:= \\
+	\$(LINUX_DIR)/net/ipv4/inet_diag.ko \\
+	\$(LINUX_DIR)/net/ipv4/tcp_diag.ko \\
+	\$(LINUX_DIR)/net/ipv4/udp_diag.ko \\
+	\$(LINUX_DIR)/net/ipv4/raw_diag.ko
+  AUTOLOAD:=\$(call AutoLoad,31,inet_diag tcp_diag udp_diag raw_diag)
 endef
 
 define KernelPackage/inet-diag/description
@@ -38,8 +38,8 @@ Support for INET (TCP, DCCP, etc) socket monitoring interface used by
 native Linux tools such as ss.
 endef
 
-$(eval $(call KernelPackage,inet-diag))
-' >>  package/kernel/linux/modules/netsupport.mk
+\$(eval \$(call KernelPackage,inet-diag))
+" >>  "/home/danshui/openwrt/package/kernel/linux/modules/netsupport.mk"
 fi
 
 if [[ `grep -c "kmod-netlink-diag" package/network/utils/iproute2/Makefile` -eq '0' ]]; then
