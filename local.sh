fb185bfd1ea83d5e32112dbe1ffed72a808dfae2
281677160 <66651997+281677160@users.noreply.github.com>
2022-11-20 01:06:17 +0800

    Update local.sh

--- local.sh
+++ local.sh
@@ -813,6 +813,7 @@ EOF
 ls -1 ${HOME_PATH} > UpdateList.txt
 FOLDERS=`grep -Fxvf UpdateList.txt Update.txt`
 FOLDERSX=`echo $FOLDERS | sed 's/ /、/g'`;echo $FOLDERSX
+rm -rf {UpdateList.txt,Update.txt}
 fi
 
 if [[ -z "${FOLDERS}" ]]; then
