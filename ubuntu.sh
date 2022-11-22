9e5bc4eed940c9690bc6a3546b92c0f85785d4ab
281677160 <66651997+281677160@users.noreply.github.com>
2022-11-22 13:27:22 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -46,9 +46,9 @@ ${INS} install -y apt-transport-https gnupg2
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get install -y nodejs gcc g++ make
-sudo apt-get update -y
-sudo apt-get install -y yarn
+${INS} install -y nodejs gcc g++ make
+${INS} update -y
+${INS} install -y yarn
 
 node --version
 yarn --version
