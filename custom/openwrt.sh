1c1c919bb29ff254e7bb97d261398d8bb9f2b609
281677160 <66651997+281677160@users.noreply.github.com>
2022-11-23 14:40:54 +0800

    Update openwrt.sh

--- custom/openwrt.sh
+++ custom/openwrt.sh
@@ -78,7 +78,7 @@ function xiugai_ip() {
     uci commit network
     ECHOG "您的IP为：${domain}"
     ECHOY "正在为您清空密码"
-    if [[ `grep -c "admin" /etc/shadow` -eq '1' ]]; then
+    if [[ "$USER" == "admin" ]]; then
       passwd -d admin
     else
       passwd -d root
@@ -97,7 +97,7 @@ while :; do
 read -p "否清空密码(shi fou qing kong mi ma)[Y/n]：" YN
 case ${YN} in
 [Yy]) 
-    if [[ `grep -c "admin" /etc/shadow` -eq '1' ]]; then
+    if [[ "$USER" == "admin" ]]; then
       passwd -d admin
     else
       passwd -d root
