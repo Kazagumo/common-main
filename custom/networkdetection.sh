e32fcf2eced2724350b307ed749781de7c352b14
281677160 <66651997+281677160@users.noreply.github.com>
2022-11-23 17:56:09 +0800

    Update networkdetection.sh

--- custom/networkdetection.sh
+++ custom/networkdetection.sh
@@ -5,7 +5,7 @@ if [[ "${wangluo}" == "1" ]]; then
   curl --connect-timeout 10 "taobao.com" > "/dev/null" 2>&1 || wangluo='2'
 fi
 if [[ "${wangluo}" == "2" ]]; then
-  curl --connect-timeout 10 "google.com" > "/dev/null" 2>&1 || wangluo='2'
+  curl --connect-timeout 10 "google.com" > "/dev/null" 2>&1 || wangluo='3'
 fi
 if [[ "${wangluo}" == "1" ]] && [[ "${wangluo}" == "2" ]] && [[ "${wangluo}" == "3" ]]; then
   echo "[没检测到网络，重启系统于$(date "+%Y年%m月%d日%H时%M分%S秒")]" >> /mnt/network.log
