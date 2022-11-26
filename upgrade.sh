0b82be86949837d471d64e2bc4a3e062592f81e6
281677160 <66651997+281677160@users.noreply.github.com>
2022-11-26 16:35:18 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -6,7 +6,7 @@
 
 function Diy_Part1() {
 	find . -name 'luci-app-autoupdate' | xargs -i rm -rf {}
-	if [[ -f "$BUILD_PATH/AutoUpdate.sh" ]]; then
+	if [[ -f "${HOME_PATH}/build/common/autoupdate/AutoUpdate.sh" ]]; then
 		echo "正在执行：给源码增加定时更新固件插件和设置插件和ttyd成默认自选"
 		git clone -b ceshi https://github.com/281677160/luci-app-autoupdate $HOME_PATH/package/luci-app-autoupdate
 		[[ ! -d "$FILES_PATH/usr/bin" ]] && mkdir -p $FILES_PATH/usr/bin
