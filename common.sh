a8cea54cdb8a766167d98c04646c5f511da71825
281677160 <66651997+281677160@users.noreply.github.com>
2022-11-26 22:31:59 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -891,21 +891,25 @@ if [[ "${Cancel_running}" == "1" ]]; then
 fi
 
 # 晶晨CPU机型自定义机型,内核,分区
-if [[ -n "${amlogic_model}" ]] && [[ "${SOURCE_CODE}" == "AMLOGIC" ]]; then
-  echo "amlogic_model=${amlogic_model}" >> ${GITHUB_ENV}
-else
-  echo "amlogic_model=s905d" >> ${GITHUB_ENV}
-fi
-if [[ -n "${amlogic_kernel}" ]] && [[ "${SOURCE_CODE}" == "AMLOGIC" ]]; then
-  echo "amlogic_kernel=${amlogic_kernel}" >> ${GITHUB_ENV}
-else
-  echo "amlogic_kernel=5.4.210_5.10.135_5.15.50" >> ${GITHUB_ENV}
-fi
-if [[ -n "${rootfs_size}" ]] && [[ "${SOURCE_CODE}" == "AMLOGIC" ]]; then
-  echo "rootfs_size=${rootfs_size}" >> ${GITHUB_ENV}
-else
-  echo "rootfs_size=960" >> ${GITHUB_ENV}
-fi
+case "${SOURCE_CODE}" in
+AMLOGIC)
+  if [[ -n "${amlogic_model}" ]]; then
+    echo "amlogic_model=${amlogic_model}" >> ${GITHUB_ENV}
+  else
+    echo "amlogic_model=s905d" >> ${GITHUB_ENV}
+  fi
+  if [[ -n "${amlogic_kernel}" ]]; then
+    echo "amlogic_kernel=${amlogic_kernel}" >> ${GITHUB_ENV}
+  else
+    echo "amlogic_kernel=5.4.210_5.10.135_5.15.50" >> ${GITHUB_ENV}
+  fi
+  if [[ -n "${rootfs_size}" ]]; then
+    echo "rootfs_size=${rootfs_size}" >> ${GITHUB_ENV}
+  else
+    echo "rootfs_size=960" >> ${GITHUB_ENV}
+  fi
+;;
+esac
 [[ -f "${GITHUB_ENV}" ]] && source ${GITHUB_ENV}
 }
 
