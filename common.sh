26c7053d539eb4608c0d758368a5283673e13323
281677160 <66651997+281677160@users.noreply.github.com>
2022-11-26 15:27:58 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -571,13 +571,13 @@ AMLOGIC)
 
   echo "
   sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
-  echo "DISTRIB_REVISION=''${LUCI_EDITION}''" >> /etc/openwrt_release
+  echo \"DISTRIB_REVISION='${LUCI_EDITION}'\" >> /etc/openwrt_release
   sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
-  echo "DISTRIB_DESCRIPTION='OpenWrt '" >> /etc/openwrt_release
+  echo \"DISTRIB_DESCRIPTION='OpenWrt '\" >> /etc/openwrt_release
   sed -i '/luciversion/d' /usr/lib/lua/luci/version.lua
-  echo "luciversion    = \"${SOURCE}\"" >> /usr/lib/lua/luci/version.lua
+  echo \"luciversion    = '${SOURCE}'\" >> /usr/lib/lua/luci/version.lua
   sed -i '/luciname/d' /usr/lib/lua/luci/version.lua
-  echo "luciname    = \"- ${LUCI_EDITION}\"" >> /usr/lib/lua/luci/version.lua
+  echo \"luciname    = '- ${LUCI_EDITION}'\" >> /usr/lib/lua/luci/version.lua
   exit 0
   " >> "${ZZZ_PATH}"
 ;;
