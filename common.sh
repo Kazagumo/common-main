40e0390eda58745825d926f1d6c307cc34593a8e
281677160 <66651997+281677160@users.noreply.github.com>
2022-11-28 01:32:42 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -587,16 +587,18 @@ echo "正在执行：给feeds.conf.default增加插件源"
 sed -i '/danshui/d' "${HOME_PATH}/feeds.conf.default"
 sed -i '/helloworld/d' "${HOME_PATH}/feeds.conf.default"
 sed -i '/passwall/d' "${HOME_PATH}/feeds.conf.default"
-echo "
+
+cat >>"${HOME_PATH}/feeds.conf.default" <<-EOF
 src-git danshui https://github.com/281677160/openwrt-package.git;${PACKAGE_BRANCH}
-" >> ${HOME_PATH}/feeds.conf.default
+EOF
+
 if [[ "$(. ${FILES_PATH}/etc/openwrt_release && echo "$DISTRIB_RECOGNIZE")" != "21" ]]; then
-echo "
+cat >>"${HOME_PATH}/feeds.conf.default" <<-EOF
 src-git helloworld https://github.com/fw876/helloworld
 src-git passwall https://github.com/xiaorouji/openwrt-passwall;packages
 src-git passwall1 https://github.com/xiaorouji/openwrt-passwall;luci
 src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2;main
-" >> ${HOME_PATH}/feeds.conf.default
+EOF
 fi
 sed -i '/^#/d' "${HOME_PATH}/feeds.conf.default"
 sed -i '/^$/d' "${HOME_PATH}/feeds.conf.default"
