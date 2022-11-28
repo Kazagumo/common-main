d31d2e5b9ce30d26cbc505819607ef1331200fa2
281677160 <66651997+281677160@users.noreply.github.com>
2022-11-28 11:24:08 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -106,7 +106,7 @@ EOF
 function Diy_Part3() {
 	BIN_PATH="${HOME_PATH}/bin/Firmware"
 	echo "BIN_PATH=${BIN_PATH}" >> ${GITHUB_ENV}
-	[[ ! -d "${BIN_PATH}" ]] && mkdir -p ${BIN_PATH} || rm -rf ${BIN_PATH}/*
+	[[ ! -d "${BIN_PATH}" ]] && mkdir -p "${BIN_PATH}" || rm -rf "${BIN_PATH}"/*
 	
 	cd "${FIRMWARE_PATH}"
 	if [[ `ls -1 |grep -c ".img"` -ge '1' ]] && [[ `ls -1 |grep -c ".img.gz"` == '0' ]]; then
@@ -118,7 +118,7 @@ function Diy_Part3() {
 		EFI_ZHONGZHUAN="$(ls -1 |egrep .*x86.*squashfs.*efi.*img.gz)"
 		if [[ -f "${EFI_ZHONGZHUAN}" ]]; then
 		  EFIMD5="$(md5sum ${EFI_ZHONGZHUAN} |cut -c1-3)$(sha256sum ${EFI_ZHONGZHUAN} |cut -c1-3)"
-		  cp "${EFI_ZHONGZHUAN}" "${BIN_PATH}/${AutoBuild_Uefi}-${EFIMD5}${Firmware_SFX}"
+		  cp -Rf "${EFI_ZHONGZHUAN}" "${BIN_PATH}/${AutoBuild_Uefi}-${EFIMD5}${Firmware_SFX}"
 		else
 		  echo "没找到uefi固件"
 		fi
@@ -138,7 +138,7 @@ function Diy_Part3() {
 		fi
 		if [[ -f "${UP_ZHONGZHUAN}" ]]; then
 		  MD5="$(md5sum ${UP_ZHONGZHUAN} | cut -c1-3)$(sha256sum ${UP_ZHONGZHUAN} | cut -c1-3)"
-		  cp "${UP_ZHONGZHUAN}" "${BIN_PATH}/${AutoBuild_Firmware}-${MD5}${Firmware_SFX}"
+		  cp -Rf "${UP_ZHONGZHUAN}" "${BIN_PATH}/${AutoBuild_Firmware}-${MD5}${Firmware_SFX}"
 		else
 		  echo "没找到固件"
 		fi
