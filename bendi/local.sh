edc56a3e2def174ba90d670a8e13eab0bad491d7
281677160 <66651997+281677160@users.noreply.github.com>
2022-11-30 01:09:49 +0800

    Update local.sh

--- bendi/local.sh
+++ bendi/local.sh
@@ -710,7 +710,7 @@ function Bendi_xuanzhe() {
   clear
   echo 
   echo
-  jixingliebiao="$(echo "0、刷新机型列表" |awk '{print "  " $0}')"
+  jixingliebiao="$(echo "0、刷新列表" |awk '{print "  " $0}')"
   ls -1 "DIY-SETUP" |awk '$0=NR" "$0' > GITHUB_ENN
   ls -1 "DIY-SETUP" > GITHUB_EVN
   XYZDSZ="$(cat GITHUB_ENN | awk 'END {print}' |awk '{print $(1)}')"
@@ -719,9 +719,7 @@ function Bendi_xuanzhe() {
   ls -1 "DIY-SETUP" |awk '$0=NR"、"$0'|awk '{print "  " $0}'
   echo
   echo
-  echo -e "${Blue}  请输入您要编译源码前面对应的数值(1~X)${Font}"
-  echo
-  echo -e "${Yellow}  输入[0]回车则刷新机型列表,输入[N]则为退出程序${Font}"
+  echo -e "${Blue}  请输入您要编译源码前面对应的数值(1~X),输入[N]则为退出程序${Font}"
   echo
   echo -e "${Green}  跟云编译一样,您可以自行在DIY-SETUP内建立机型文件夹来进行编译使用(不懂的请查看云编译教程)${Font}"
   echo
