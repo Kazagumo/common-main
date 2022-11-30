fbbdbc9835cc4013813a90137bab2701da4f8afd
281677160 <66651997+281677160@users.noreply.github.com>
2022-12-01 00:35:02 +0800

    Update local.sh

--- bendi/local.sh
+++ bendi/local.sh
@@ -289,7 +289,9 @@ ECHOGG "检测是否缺少文件"
 source common.sh && Diy_settings
 [[ -f "${DEFAULT_PATH}" ]] && source common.sh && Diy_wenjian
 echo
-cp -rf ${GITHUB_WORKSPACE}/build/common/*.sh build/${FOLDER_NAME}/
+cp -Rf build/common.sh build/${FOLDER_NAME}/common.sh
+cp -Rf build/upgrade.sh build/${FOLDER_NAME}/upgrade.sh
+cp -Rf ${GITHUB_WORKSPACE}/build/common/*.sh build/${FOLDER_NAME}/
 sudo chmod -R +x build
 }
 
