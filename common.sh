c1d8fc56ba22475d45262135c7d858bb4e85a2e9
281677160 <66651997+281677160@users.noreply.github.com>
2022-12-02 00:12:37 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -548,8 +548,8 @@ function Diy_distrib() {
 echo "正在执行：烦恼"
 cd ${HOME_PATH}
 export ZZZ_PAT=$(find . -type f -name '*default-settings' |grep 'package' |grep 'files')
-echo "${ZZZ_PATH}"
-export ZZZ_PATH="$(find . -type f -name '*default-settings' |grep 'package' |grep 'files')"
+echo "${ZZZ_PAT}"
+export ZZZ_PATH=$(find ${HOME_PATH} -type f -name '*default-settings' |grep 'package' |grep 'files')
 echo "${ZZZ_PATH}"
 echo "ZZZ_PATH=${ZZZ_PATH}" >> ${GITHUB_ENV}
 echo "${ZZZ_PATH}"
