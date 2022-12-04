6d459a52e6b373abc388dec439a9ef097de90861
281677160 <66651997+281677160@users.noreply.github.com>
2022-12-05 00:36:04 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -23,6 +23,51 @@ Compte=$(date +%Y年%m月%d号%H时%M分)
 }
 
 
+function settings_variable() {
+        export ymlpath="build/${FOLDER_NAME}/settings.ini"
+        if [[ "${{ github.event.inputs.INFORMATION_NOTICE }}" == '关闭' ]]; then
+          export INFORMATION_NOTICE2="INFORMATION_NOTICE\\=\\\"false\\\""
+        elif [[ "${{ github.event.inputs.INFORMATION_NOTICE }}" == 'Telegram' ]]; then
+          export INFORMATION_NOTICE2="INFORMATION_NOTICE\\=\\\"TG\\\""
+        elif [[ "${{ github.event.inputs.INFORMATION_NOTICE }}" == 'pushplus' ]]; then
+          export INFORMATION_NOTICE2="INFORMATION_NOTICE\\=\\\"PUSH\\\""
+        fi
+        
+        if [[ -n "${{ github.event.inputs.REPO_BRANCH }}" ]]; then
+          SOURCE_CODE1="$(grep "SOURCE_CODE=" "${ymlpath}" |sed 's/^[ ]*//g' |grep -v '^#' |awk '{print $(1)}' |sed 's?=?\\&?g' |sed 's?"?\\&?g')"
+          REPO_BRANCH1="$(grep "REPO_BRANCH=" "${ymlpath}" |sed 's/^[ ]*//g' |grep -v '^#' |awk '{print $(1)}' |sed 's?=?\\&?g' |sed 's?"?\\&?g')"
+          CONFIG_FILE1="$(grep "CONFIG_FILE=" "${ymlpath}" |sed 's/^[ ]*//g' |grep -v '^#' |awk '{print $(1)}' |sed 's?=?\\&?g' |sed 's?"?\\&?g')"
+          UPLOAD_FIRMWARE1="$(grep "UPLOAD_FIRMWARE=" "${ymlpath}" |sed 's/^[ ]*//g' |grep -v '^#' |awk '{print $(1)}' |sed 's?=?\\&?g' |sed 's?"?\\&?g')"
+          UPLOAD_RELEASE1="$(grep "UPLOAD_RELEASE=" "${ymlpath}" |sed 's/^[ ]*//g' |grep -v '^#' |awk '{print $(1)}' |sed 's?=?\\&?g' |sed 's?"?\\&?g')"
+          CACHEWRTBUILD_SWITCH1="$(grep "CACHEWRTBUILD_SWITCH=" "${ymlpath}" |sed 's/^[ ]*//g' |grep -v '^#' |awk '{print $(1)}' |sed 's?=?\\&?g' |sed 's?"?\\&?g')"
+          UPDATE_FIRMWARE_ONLINE1="$(grep "UPDATE_FIRMWARE_ONLINE=" "${ymlpath}" |sed 's/^[ ]*//g' |grep -v '^#' |awk '{print $(1)}' |sed 's?=?\\&?g' |sed 's?"?\\&?g')"
+          COLLECTED_PACKAGES1="$(grep "COLLECTED_PACKAGES=" "${ymlpath}" |sed 's/^[ ]*//g' |grep -v '^#' |awk '{print $(1)}' |sed 's?=?\\&?g' |sed 's?"?\\&?g')"
+          CPU_SELECTION1="$(grep "CPU_SELECTION=" "${ymlpath}" |sed 's/^[ ]*//g' |grep -v '^#' |awk '{print $(1)}' |sed 's?=?\\&?g' |sed 's?"?\\&?g')"
+          INFORMATION_NOTICE1="$(grep "INFORMATION_NOTICE=" "${ymlpath}" |sed 's/^[ ]*//g' |grep -v '^#' |awk '{print $(1)}' |sed 's?=?\\&?g' |sed 's?"?\\&?g')"
+          
+          export SOURCE_CODE2="SOURCE_CODE\\=\\\"IMMORTALWRT\\\""
+          export REPO_BRANCH2="REPO_BRANCH\\=\\\"${{ github.event.inputs.REPO_BRANCH }}\\\""
+          export CONFIG_FILE2="CONFIG_FILE\\=\\\"Configs/${{ github.event.inputs.CONFIG_FILE }}\\\""
+          export UPLOAD_FIRMWARE2="UPLOAD_FIRMWARE\\=\\\"${{ github.event.inputs.UPLOAD_FIRMWARE }}\\\""
+          export UPLOAD_RELEASE2="UPLOAD_RELEASE\\=\\\"${{ github.event.inputs.UPLOAD_RELEASE }}\\\""
+          export CACHEWRTBUILD_SWITCH2="CACHEWRTBUILD_SWITCH\\=\\\"${{ github.event.inputs.CACHEWRTBUILD_SWITCH }}\\\""
+          export UPDATE_FIRMWARE_ONLINE2="UPDATE_FIRMWARE_ONLINE\\=\\\"${{ github.event.inputs.UPDATE_FIRMWARE_ONLINE }}\\\""
+          export COLLECTED_PACKAGES2="COLLECTED_PACKAGES\\=\\\"${{ github.event.inputs.COLLECTED_PACKAGES }}\\\""
+          export CPU_SELECTION2="CPU_SELECTION\\=\\\"${{ github.event.inputs.CPU_SELECTION }}\\\""
+        
+          sed -i "s?${SOURCE_CODE1}?${SOURCE_CODE2}?g" build/${FOLDER_NAME}/settings.ini
+          sed -i "s?${REPO_BRANCH1}?${REPO_BRANCH2}?g" build/${FOLDER_NAME}/settings.ini
+          sed -i "s?${CONFIG_FILE1}?${CONFIG_FILE2}?g" build/${FOLDER_NAME}/settings.ini
+          sed -i "s?${UPLOAD_FIRMWARE1}?${UPLOAD_FIRMWARE2}?g" build/${FOLDER_NAME}/settings.ini
+          sed -i "s?${UPLOAD_RELEASE1}?${UPLOAD_RELEASE2}?g" build/${FOLDER_NAME}/settings.ini
+          sed -i "s?${CACHEWRTBUILD_SWITCH1}?${CACHEWRTBUILD_SWITCH2}?g" build/${FOLDER_NAME}/settings.ini
+          sed -i "s?${INFORMATION_NOTICE1}?${INFORMATION_NOTICE2}?g" build/${FOLDER_NAME}/settings.ini
+          sed -i "s?${COLLECTED_PACKAGES1}?${COLLECTED_PACKAGES2}?g" build/${FOLDER_NAME}/settings.ini
+          sed -i "s?${CPU_SELECTION1}?${CPU_SELECTION2}?g" build/${FOLDER_NAME}/settings.ini
+          sed -i "s?${INFORMATION_NOTICE1}?${INFORMATION_NOTICE2}?g" build/${FOLDER_NAME}/settings.ini
+        fi
+}
+
 function Diy_variable() {
 if [[ -n "${BENDI_VERSION}" ]]; then
   source "${GITHUB_WORKSPACE}/DIY-SETUP/${FOLDER_NAME}/settings.ini"
