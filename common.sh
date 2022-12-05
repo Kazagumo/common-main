bf282358507f3ed302a7f1b25dedf843c43c8a6a
281677160 <66651997+281677160@users.noreply.github.com>
2022-12-05 20:51:42 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -1422,11 +1422,12 @@ else
 fi
 
 if [[ "${Continue_selecting}" == "1" ]]; then
+  cd ${GITHUB_WORKSPACE}
   git clone -b main https://github.com/${GIT_REPOSITORY}.git ${FOLDER_NAME}
   rm -rf ${FOLDER_NAME}/build/${FOLDER_NAME}
   cp -Rf build/${FOLDER_NAME} ${FOLDER_NAME}/build/${FOLDER_NAME}
   
-  rm -rf .github/workflows
+  rm -rf ${FOLDER_NAME}/.github/workflows
   cp -Rf .github/workflows ${FOLDER_NAME}/.github/workflows
   
   if [[ `ls -1 "${FOLDER_NAME}/build/${FOLDER_NAME}/start-up" |grep -Eoc '[0-9]+\.ini'` -eq '1' ]]; then
