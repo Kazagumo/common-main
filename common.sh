6498d7e3a4f3611a94c7a658938ecd0288dcc49c
281677160 <66651997+281677160@users.noreply.github.com>
2022-12-07 13:38:58 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -1120,8 +1120,14 @@ if [[ ! "${Required_Topic}" == "0" ]] && [[ -n "${Required_Topic}" ]]; then
   collections="${HOME_PATH}/feeds/luci/collections/luci/Makefile"
   ybtheme="$(grep -Eo "luci-theme-.*" "${collections}" |sed -r 's/.*theme-(.*)=y/\1/' |awk '{print $(1)}')"
   yhtheme="luci-theme-${Required_Topic}"
-  aa=$(grep -i "=${Required_Topic}" -rl "${HOME_PATH}" |grep Makefile)
-  if [[ -n "${aa}" ]]; then
+  for X in $(grep -i "=${Required_Topic}" -rl "${HOME_PATH}" |grep Makefile); do 
+    if [[ `grep -ic "=${Required_Topic}" "${X}"` -eq '0' ]]; then
+      cc="0"
+    else
+      cc="1"
+   fi
+  done
+  if [[ "${cc}" == '1' ]]; then
     sed -i "s/${ybtheme}/${yhtheme}/g" "${collections}"
   else
     echo "TIME r \"没有${yhtheme}此主题存在,不进行替换${ybtheme}主题操作\"" >> ${HOME_PATH}/CHONGTU
