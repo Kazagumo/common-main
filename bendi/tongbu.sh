4311f6bd1494a5741b391e43cadda9557754ce2d
281677160 <66651997+281677160@users.noreply.github.com>
2022-12-09 22:54:34 +0800

    Update tongbu.sh

--- bendi/tongbu.sh
+++ bendi/tongbu.sh
@@ -53,45 +53,25 @@ case "${TONGBU_CANGKU}" in
     fi
     if [[ "${SOURCE_CODE1}" == "AMLOGIC" ]] && [[ "${aa}" == "${aa}" ]]; then
       cp -Rf ${GITHUB_WORKSPACE}/shangyou/.github/workflows/Amlogic.yml ${X}
-      yml_name2="$(grep 'name:' "${X}"  |grep -v '^#' |awk 'NR==1')"
-      echo "${yml_name2}"
-      TARGE1="$(grep 'target: \[' "${X}" |sed 's/^[ ]*//g' |grep -v '^#' |sed 's/\[/\\&/' |sed 's/\]/\\&/')"
-      echo "${TARGE1}"
-      sed -i "s?${TARGE1}?${TARGE2}?g" "${X}"
-      sed -i "s?${yml_name2}?${yml_name1}?g" "${X}"
     elif [[ "${SOURCE_CODE1}" == "IMMORTALWRT" ]]; then
       cp -Rf ${GITHUB_WORKSPACE}/shangyou/.github/workflows/Immortalwrt.yml ${X}
-      yml_name2="$(grep 'name:' "${X}"  |grep -v '^#' |awk 'NR==1')"
-      TARGE1="$(grep 'target: \[' "${X}" |sed 's/^[ ]*//g' |grep -v '^#' |sed 's/\[/\\&/' |sed 's/\]/\\&/')"
-      sed -i "s?${TARGE1}?${TARGE2}?g" "${X}"
-      sed -i "s?${yml_name2}?${yml_name1}?g" "${X}"
     elif [[ "${SOURCE_CODE1}" == "COOLSNOWWOLF" ]] && [[ "${aa}" == "${aa}" ]]; then
       cp -Rf ${GITHUB_WORKSPACE}/shangyou/.github/workflows/Lede.yml ${X}
-      yml_name2="$(grep 'name:' "${cc}"  |grep -v '^#' |awk 'NR==1')"   
-      TARGE1="$(grep 'target: \[' "${X}" |sed 's/^[ ]*//g' |grep -v '^#' |sed 's/\[/\\&/' |sed 's/\]/\\&/')"
-      sed -i "s?${TARGE1}?${TARGE2}?g" "${X}"
-      sed -i "s?${yml_name2}?${yml_name1}?g" "${X}"
     elif [[ "${SOURCE_CODE1}" == "LIENOL" ]]; then 
       cp -Rf ${GITHUB_WORKSPACE}/shangyou/.github/workflows/Lienol.yml ${X}
-      yml_name2="$(grep 'name:' "${X}"  |grep -v '^#' |awk 'NR==1')" 
-      TARGE1="$(grep 'target: \[' "${X}" |sed 's/^[ ]*//g' |grep -v '^#' |sed 's/\[/\\&/' |sed 's/\]/\\&/')"
-      sed -i "s?${TARGE1}?${TARGE2}?g" "${X}"
-      sed -i "s?${yml_name2}?${yml_name1}?g" "${X}"
     elif [[ "${SOURCE_CODE1}" == "OFFICIAL" ]]; then
       cp -Rf ${GITHUB_WORKSPACE}/shangyou/.github/workflows/Official.yml ${X}
-      yml_name2="$(grep 'name:' "${X}"  |grep -v '^#' |awk 'NR==1')" 
-      TARGE1="$(grep 'target: \[' "${X}" |sed 's/^[ ]*//g' |grep -v '^#' |sed 's/\[/\\&/' |sed 's/\]/\\&/')"
-      sed -i "s?${TARGE1}?${TARGE2}?g" "${X}"
-      sed -i "s?${yml_name2}?${yml_name1}?g" "${X}"
     elif [[ "${SOURCE_CODE1}" == "XWRT" ]]; then 
       cp -Rf ${GITHUB_WORKSPACE}/shangyou/.github/workflows/Xwrt.yml ${X}
-      yml_name2="$(grep 'name:' "${X}"  |grep -v '^#' |awk 'NR==1')" 
-      TARGE1="$(grep 'target: \[' "${X}" |sed 's/^[ ]*//g' |grep -v '^#' |sed 's/\[/\\&/' |sed 's/\]/\\&/')"
-      sed -i "s?${TARGE1}?${TARGE2}?g" "${X}"
-      sed -i "s?${yml_name2}?${yml_name1}?g" "${X}"
     else
       echo "${SOURCE_CODE1}无此文件"
     fi
+      yml_name2="$(grep 'name:' "${X}"  |grep -v '^#' |awk 'NR==1')"
+      echo "${yml_name2}"
+      TARGE1="target: \\[.*\\]"
+      TARGE2="target: \\[${aa}\\]"
+      sed -i "s?${TARGE1}?${TARGE2}?g" ${X}
+      sed -i "s?${yml_name2}?${yml_name1}?g" "${X}"
   done
   
   cp -Rf ${GITHUB_WORKSPACE}/shangyou/.github/workflows/compile.yml ${GITHUB_WORKSPACE}/repogx/.github/workflows/compile.yml
