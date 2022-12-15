4881036cbc4c1c16c34e73ca4a5ab2ab19a6f460
281677160 <66651997+281677160@users.noreply.github.com>
2022-12-16 00:58:46 +0800

    Update tongbu.sh

--- bendi/tongbu.sh
+++ bendi/tongbu.sh
@@ -110,9 +110,12 @@ fi
 
 # 恢复settings.ini设置
 for X in $(find "operates" -type f -name "diy-part.sh" |grep -v '.bak'); do
+  echo "${X}"
   # 后台IP设置
   aa="$(source "${X}" && echo "${Ipv4_ipaddr}")"
+  echo "${aa}"
   bb="$(source "${X}.bak" && echo "${Ipv4_ipaddr}")"
+  echo "${bb}"
   if [[ -n "${aa}" ]] && [[ -n "${bb}" ]]; then
     sed -i "s?${aa}?${bb}?g" "${X}"
   fi
