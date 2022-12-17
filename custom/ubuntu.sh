9ba7329b8f3e1086e22b2eac3a8baae85c057946
281677160 <66651997+281677160@users.noreply.github.com>
2022-12-17 17:36:55 +0800

    Update ubuntu.sh

--- custom/ubuntu.sh
+++ custom/ubuntu.sh
@@ -9,24 +9,37 @@ if [[ "${SOURCE_CODE}" == "AMLOGIC" ]]; then
   docker rmi `docker images -q`
   sudo -E apt-get -qq remove -y --purge azure-cli ghc* zulu* llvm* firefox google* powershell openjdk* msodbcsql17 mongodb* moby* snapd* mysql*
 fi
-sudo apt-get -y remove gcc g++
 sudo rm -rf /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /usr/lib/jvm /opt/ghc /swapfile
 }
 
 function install_mustrelyon(){
 # 安装大雕列出的编译openwrt依赖
-sudo bash -c 'bash <(curl -s https://build-scripts.immortalwrt.eu.org/init_build_environment.sh)'
+${INS} update -y
+${INS} full-upgrade -y
+${INS} install -y ack antlr3 aria2 asciidoc autoconf automake autopoint binutils bison build-essential \
+bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-multilib g++-multilib \
+git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev \
+libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libreadline-dev libssl-dev libtool lrzsz \
+mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 python3-pip libpython3-dev qemu-utils \
+rsync scons squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev
 }
 
 function ophub_amlogic-s9xxx(){
 # 安装我仓库需要的依赖
-${INS} install -y rename pigz libfuse-dev
+${INS} install -y rename pigz
 # 安装打包N1需要用到的依赖
-# ${INS} install -y $(curl -fsSL https://is.gd/depend_ubuntu2204_openwrt)
+${INS} install -y $(curl -fsSL https://is.gd/depend_ubuntu2204_openwrt)
 }
 
 function update_apt_source(){
 # 安装nodejs 16 和yarn
+${INS} install -y apt-transport-https gnupg2
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+${INS} install -y nodejs gcc g++ make
+${INS} update -y
+${INS} install -y yarn
 
 node --version
 yarn --version
