72d91331cd52a1d6d2a655eb9f77eb0ff64e7efe
281677160 <66651997+281677160@users.noreply.github.com>
2022-12-18 20:47:34 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -921,8 +921,10 @@ if [[ -d "${HOME_PATH}/package/luci-app-openclash" ]]; then
 fi
 if [[ "${HOUR}" -lt "12" ]]; then
   clashgs="1"
+  echo "OpenClash_Core=0" >> ${GITHUB_ENV}
 else
   clashgs="0"
+  echo "OpenClash_Core=${OpenClash_Core}" >> ${GITHUB_ENV}
 fi
 if [[ -f "${jiance_clash}" ]]; then
   clash_branch="$(cat ${jiance_clash})"
