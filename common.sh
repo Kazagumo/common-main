2b6eb600e67c03816a2ee2f292228bc978522e27
281677160 <66651997+281677160@users.noreply.github.com>
2023-01-10 14:14:28 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -512,6 +512,8 @@ master)
   sed -i '/DISTRIB_RECOGNIZE/d' "${REPAIR_PATH}"
   echo -e "\nDISTRIB_RECOGNIZE='18'" >> "${REPAIR_PATH}" && sed -i '/^\s*$/d' "${REPAIR_PATH}"
   curl -fsSL https://raw.githubusercontent.com/281677160/common-main/main/LIENOL/master/boots > ${HOME_PATH}/feeds/packages/libs/boost/Makefile
+  rm -rf ${HOME_PATH}/package/network/services/dnsmasq
+  svn export https://github.com/coolsnowwolf/lede/trunk/package/network/services/dnsmasq ${HOME_PATH}/package/network/services/dnsmasq
 
 ;;
 21.02)
